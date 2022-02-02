061c676b10843486c4ad029718cfb1b8e6c0c037
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-02-01 22:10:28 -0300

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,6 @@
 export default {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
