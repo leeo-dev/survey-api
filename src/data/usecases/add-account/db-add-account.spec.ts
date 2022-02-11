82240cf1654f4c569ac7b27a382a035044b4121e
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-02-10 22:38:20 -0300

    refacotr: move sut creation to a factory help method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,14 +1,26 @@
+import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
+
+interface sutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): sutTypes => {
+  class EncrypterStub {
+    async encrypt (value: string): Promise<string> {
+      return await Promise.resolve('hashed_value')
+    }
+  }
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+  return { encrypterStub, sut }
+}
+
 describe('DbAddAccount Usecase', () => {
   test('Should call Encrypter with correct password ', async () => {
-    class EncryptStub {
-      async encrypt (value: string): Promise<string> {
-        return await Promise.resolve('hashed_value')
-      }
-    }
-    const encryptStub = new EncryptStub()
-    const sut = new DbAddAccount(encryptStub)
-    const encryptSpy = jest.spyOn(encryptStub, 'encrypt')
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
       email: 'valid_email@mail.com',
